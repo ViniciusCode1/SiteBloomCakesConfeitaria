c6f6e35240ff4bf12f5e59d674b0c80501d63961
Fabricio Bonfim <fabriciobds223@gmail.com>
2025-02-27 20:03:12 -0300

    javascript

---
+++ Html/script.js
@@ -0,0 +1,15 @@
+const username = document.getElementById("nome_cliente")
+const email = document.getElementById("email")
+const CEP_cliente = document.getElementById("CEP_cliente")
+const doces = document.getElementById("doces")
+const sabor = document.getElementById("sabor")
+const quantidade = document.getElementById("quantidade")
+const pagamento = document.getElementById("pagamento");
+
+
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefa();
+    
+    alert("SUCESSO")
+})
